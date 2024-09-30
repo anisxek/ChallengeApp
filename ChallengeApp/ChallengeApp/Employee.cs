36d3495567ed5d3fde97f762e5bb994db3ba8159
anisxek <martuchaaniszek1@gmail.com>
2024-10-01 01:25:43 +0200

    Praca Domowa Dzień 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        
+        private List<int> score = new List<int>();
+        public Employee(string name, string lastname, int age, int result)
+        {
+            this.Name = name;
+            this.LastName = lastname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+       
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+
+    }
+
+}
+
