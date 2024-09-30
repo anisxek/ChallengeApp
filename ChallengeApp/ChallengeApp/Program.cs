65165b318f41e2f86bcf4a519a366df1d87d65e4
anisxek <martuchaaniszek1@gmail.com>
2024-09-30 18:48:46 +0200

    Praca Domowa Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "kobieta";
+var age = 30;
+
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa 30 lat");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("");
+}
